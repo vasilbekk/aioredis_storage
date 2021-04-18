1df7f327d06dded93f478c763c933b53ca37909f
vasilbekk *macbook <vasilbekk@mail.ru>
2021-04-18 08:21:22 +0300

    Initial package

---
+++ storage.py
@@ -0,0 +1,41 @@
+from typing import Any
+
+import aioredis
+
+from .mixins import PickleMixin
+
+
+class RedisStorage(PickleMixin):
+
+	def __init__(self,
+				db_name: str = 'localhost',
+				db_port: int = 6379,
+				db_number: int = None,
+				password: int = None):
+		self._name = db_name
+		self._port = db_port
+		self._number = db_number
+		self._password = password
+		self._redis = None
+
+	async def redis(self):
+		if not self._redis:
+			self._redis = await aioredis.create_connection(
+				(self._name, self._port),
+				db = self._number,
+				password = self._password
+				)
+
+		return self._redis
+
+	async def set_data(self, address: str, data: Any):
+		conn = await self.redis()
+		data = await self.dump(data)
+		await conn.execute('SET', address, data)
+
+	async def get_data(self, address: str):
+		conn = await self.redis()
+
+		raw_data = await conn.execute('GET', address)
+		data = await self.load(raw_data)
+		return data
