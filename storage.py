729dfdc4b4588cf238938f0c62d189b8feed6dc4
vasilbekk *macbook <vasilbekk@mail.ru>
2021-04-18 09:36:59 +0300

    Add new methods: get_all_adrresses and reset_all

--- storage.py
+++ storage.py
@@ -1,11 +1,13 @@
 from abc import ABC
-from typing import Any
+from typing import Any, List
 
 import aioredis
 
 from .mixins import JSONMixin, PickleMixin
 
 DEFAULT_PREFIX = 'rs'
+DEFAULT_SEPARATOR = ':'
+
 
 class AbstractRedisStorage(ABC):
 
@@ -14,12 +16,14 @@ class AbstractRedisStorage(ABC):
 				db_port: int = 6379,
 				db_number: int = None,
 				password: int = None,
-				prefix: str = DEFAULT_PREFIX):
+				prefix: str = DEFAULT_PREFIX,
+				separator: str = DEFAULT_SEPARATOR):
 		self._name = db_name
 		self._port = db_port
 		self._number = db_number
 		self._password = password
 		self._prefix = prefix
+		self._separator = separator
 		self._redis = None
 
 
@@ -34,7 +38,7 @@ class AbstractRedisStorage(ABC):
 		return self._redis
 
 	async def get_address(self, address: str) -> str:
-		return '%s:%s' % (self._prefix, address)
+		return '%s%s%s' % (self._prefix, self._separator, address)
 
 	async def set_data(self, address: str, data: Any):
 		conn = await self.redis()
@@ -44,6 +48,29 @@ class AbstractRedisStorage(ABC):
 		conn = await self.redis()
 		raw_data = await conn.execute('GET', await self.get_address(address))
 		return await self.load(raw_data)
+
+	async def get_all_addresses(self):
+		conn = await self.redis()
+		keys = await conn.execute('KEYS', f'{self._prefix}:*')
+		result = []
+		for key in keys:
+			*_, address = key.decode('utf-8').split(self._separator)
+			result.append(address)
+
+		return result
+
+	async def reset_all(self, full=False) -> List[str]:
+		conn = await self.redis()
+
+		if full:
+			await conn.execute('FLUSHDB')
+		else:
+			addr = await self.get_address('*')
+			keys = await conn.execute('KEYS', addr)
+			await conn.execute('DEL', *keys)
+
+		return keys
+
 
 
 class RedisStorage(AbstractRedisStorage, PickleMixin):
