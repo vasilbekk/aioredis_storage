e194894b7a3dda43ebe273ccfb5de5888987eddd
vasilbekk <vasilbekk@mail.ru>
2022-10-01 16:59:28 +0300

    Formatted

--- mixins.py
+++ mixins.py
@@ -4,54 +4,51 @@ from abc import ABC, abstractmethod
 
 
 class AbstractMixin(ABC):
-	"""
-		Абстрактный класс для наследования 
-		И создания новых способов обработки данных
-	"""
+    """
+    Абстрактный класс для наследования
+    И создания новых способов обработки данных
+    """
 
-	@abstractmethod
-	async def load(self, data):
-		"""
-			Получает данные, возвращенные хранилищем
-			Возвращает обаботанные данные.
-	
-			Вызывается после того, как данные были получены из хранилища
-		"""
-		raise NotImplementedError
+    @abstractmethod
+    async def load(self, data):
+        """
+        Получает данные, возвращенные хранилищем
+        Возвращает обаботанные данные.
 
-	@abstractmethod
-	async def dump(self, data):
-		"""
-			Получает данные, котороые необходимо преобразовать
-			Возвращает преобразованные данные для хранилища.
+        Вызывается после того, как данные были получены из хранилища
+        """
+        raise NotImplementedError
 
-			Вызывается перед тем, как положить данные в хранилище
-		"""
-		raise NotImplementedError
+    @abstractmethod
+    async def dump(self, data):
+        """
+        Получает данные, котороые необходимо преобразовать
+        Возвращает преобразованные данные для хранилища.
+
+        Вызывается перед тем, как положить данные в хранилище
+        """
+        raise NotImplementedError
 
 
 class BaseMixin(AbstractMixin):
+    async def load(self, data):
+        return data
 
-	async def load(self, data):
-		return data
-
-	async def dump(self, data):
-		return data
+    async def dump(self, data):
+        return data
 
 
 class JSONMixin(AbstractMixin):
+    async def load(self, data) -> object:
+        return json.loads(data)
 
-	async def load(self, data) -> object:
-		return json.loads(data)
-
-	async def dump(self, data) -> str:
-		return json.dumps(data)
+    async def dump(self, data) -> str:
+        return json.dumps(data)
 
 
 class PickleMixin(AbstractMixin):
+    async def load(self, data) -> object:
+        return pickle.loads(data)
 
-	async def load(self, data) -> object:
-		return pickle.loads(data)
-
-	async def dump(self, data) -> str:
-		return pickle.dumps(data)
+    async def dump(self, data) -> str:
+        return pickle.dumps(data)
--- storage.py
+++ storage.py
@@ -5,85 +5,82 @@ import aioredis
 
 from .mixins import JSONMixin, PickleMixin
 
-DEFAULT_PREFIX = 'rs'
-DEFAULT_SEPARATOR = ':'
+DEFAULT_PREFIX = "rs"
+DEFAULT_SEPARATOR = ":"
 
 
 class AbstractRedisStorage(ABC):
+    def __init__(
+        self,
+        db_name: str = "localhost",
+        db_port: int = 6379,
+        db_number: int = None,
+        password: int = None,
+        prefix: str = DEFAULT_PREFIX,
+        separator: str = DEFAULT_SEPARATOR,
+    ):
+        self._name = db_name
+        self._port = db_port
+        self._number = db_number
+        self._password = password
+        self._prefix = prefix
+        self._separator = separator
+        self._redis = None
 
-	def __init__(self,
-				db_name: str = 'localhost',
-				db_port: int = 6379,
-				db_number: int = None,
-				password: int = None,
-				prefix: str = DEFAULT_PREFIX,
-				separator: str = DEFAULT_SEPARATOR):
-		self._name = db_name
-		self._port = db_port
-		self._number = db_number
-		self._password = password
-		self._prefix = prefix
-		self._separator = separator
-		self._redis = None
+    async def redis(self):
+        if not self._redis:
+            self._redis = await aioredis.create_connection(
+                (self._name, self._port), db=self._number, password=self._password
+            )
 
+        return self._redis
 
-	async def redis(self):
-		if not self._redis:
-			self._redis = await aioredis.create_connection(
-				(self._name, self._port),
-				db = self._number,
-				password = self._password
-				)
+    async def close(self):
+        if self._redis and not self._redis.closed:
+            self._redis.close()
 
-		return self._redis
+    async def get_address(self, key: str) -> str:
+        return "%s%s%s" % (self._prefix, self._separator, key)
 
-	async def close(self):
-		if self._redis and not self._redis.closed:
-			self._redis.close()
+    async def set_data(self, key: str, data: Any):
+        conn = await self.redis()
+        await conn.execute("SET", await self.get_address(key), await self.dump(data))
 
-	async def get_address(self, key: str) -> str:
-		return '%s%s%s' % (self._prefix, self._separator, key)
+    async def get_data(self, key: str):
+        conn = await self.redis()
+        raw_data = await conn.execute("GET", await self.get_address(key))
+        return await self.load(raw_data)
 
-	async def set_data(self, key: str, data: Any):
-		conn = await self.redis()
-		await conn.execute('SET', await self.get_address(key), await self.dump(data))
+    async def all_keys(self):
+        conn = await self.redis()
+        keys = await conn.execute("KEYS", f"{self._prefix}:*")
+        result = []
+        for key in keys:
+            *_, key = key.decode("utf-8").split(self._separator)
+            result.append(key)
 
-	async def get_data(self, key: str):
-		conn = await self.redis()
-		raw_data = await conn.execute('GET', await self.get_address(key))
-		return await self.load(raw_data)
+        return result
 
-	async def all_keys(self):
-		conn = await self.redis()
-		keys = await conn.execute('KEYS', f'{self._prefix}:*')
-		result = []
-		for key in keys:
-			*_, key = key.decode('utf-8').split(self._separator)
-			result.append(key)
+    async def reset_all(self, full=False) -> List[str]:
+        conn = await self.redis()
 
-		return result
+        if full:
+            await conn.execute("FLUSHDB")
+        else:
+            addr = await self.get_address("*")
+            keys = await conn.execute("KEYS", addr)
+            await conn.execute("DEL", *keys)
 
-	async def reset_all(self, full=False) -> List[str]:
-		conn = await self.redis()
+        return keys
 
-		if full:
-			await conn.execute('FLUSHDB')
-		else:
-			addr = await self.get_address('*')
-			keys = await conn.execute('KEYS', addr)
-			await conn.execute('DEL', *keys)
-
-		return keys
-
-	async def reset(self, *keys):
-		conn = await self.redis()
-		await conn.execute('DEL', *keys)
-
-
+    async def reset(self, *keys):
+        conn = await self.redis()
+        await conn.execute("DEL", *keys)
 
 
 class RedisStorage(AbstractRedisStorage, PickleMixin):
-	pass
+    pass
+
 
 class RedisJSONStorage(AbstractRedisStorage, JSONMixin):
-	pass
+    pass
